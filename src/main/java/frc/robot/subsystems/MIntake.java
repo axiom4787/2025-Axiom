cc7808146b2e522916904f1d879fd4dcb27c781f
Tanmay <mail2tanmaygarg@gmail.com>
2025-01-08 20:38:39 -0600

    initial commit part 2
    basic structure

---
+++ src/main/java/frc/robot/subsystems/MIntake.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class MIntake extends SubsystemBase{
+    public MIntake() {
+        // instantiate the 4 motors
+    }
+    // state 1 -- L1
+    // state 2 -- L2
+    // state 3 -- L3
+    // state 4 -- L4
+    // state 5 -- Coral Intake
+    // state 6 -- Algae Intake
+    // state 7 -- processor
+}
