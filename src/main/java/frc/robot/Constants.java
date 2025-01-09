23ed74f36e063f6e7222ec008a26ff52c783909d
Angelo <102323862+agaur12@users.noreply.github.com>
2025-01-09 17:16:26 -0600

    Added code skeleton & constants & switches

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import org.ejml.equation.Variable;
+
+public class Constants {
+    public enum Machine{
+        L1,
+        L2,
+        L3,
+        L4,
+        CoralIntake,
+        AlgaeIntake,
+        Processor,
+        Ground,
+        Climbing_on,
+        Climbing_off,
+        Robot_off
+    }
+    public static class Arm_Angles{
+        public static final float arm_angle_l1 = 0; // Check
+        public static final float arm_angle_l2 = 0; // Check
+        public static final float arm_angle_l3 = 0; // Check
+        public static final float arm_angle_l4 = 0; // Check
+    }
+}
--- src/main/java/frc/robot/subsystems/MIntake.java
+++ src/main/java/frc/robot/subsystems/MIntake.java
@@ -1,28 +1,77 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import javax.accessibility.AccessibleRelation;
 
-enum Level{
-    Processor,
-    L1,
-    L2,
-    L3,
-    L4
-}
-enum Intake_Mode{
-    Cl_intake,
-    Algae_intake
-}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Machine;
+import frc.robot.Constants.Arm_Angles;
+
 
 public class MIntake extends SubsystemBase{
+
+    // Replace with the current state global via import later
+    public Machine mState = Machine.Robot_off;
+
     public MIntake() {
         // instantiate the 4 motors
     }
-    // state 1 -- L1
-    // state 2 -- L2
-    // state 3 -- L3
-    // state 4 -- L4
-    // state 5 -- Coral Intake
-    // state 6 -- Algae Intake
-    // state 7 -- processor
+    
+    public class Actions {
+        public static void Intake_Algae(){
+
+        }
+        public static void Outtake_Algae(){
+    
+        }
+        public static void Angle_Arm(float angle){
+    
+        }
+        public static void Intake_Coral(){
+    
+        }
+        public static void Outtake_Coral(){
+    
+        }
+    }
+    public void periodic(){
+        switch(mState){
+            case L1:
+                Actions.Angle_Arm(Arm_Angles.arm_angle_l1);
+                Actions.Outtake_Coral();
+            case L2:
+                Actions.Angle_Arm(Arm_Angles.arm_angle_l2);
+                Actions.Outtake_Coral();
+                Actions.Intake_Algae();
+            case L3:
+                Actions.Angle_Arm(Arm_Angles.arm_angle_l3);
+                Actions.Outtake_Coral();
+                Actions.Intake_Algae();
+            case L4:
+                Actions.Angle_Arm(Arm_Angles.arm_angle_l4);
+                Actions.Outtake_Coral();
+            
+            case CoralIntake:
+                Actions.Intake_Coral();
+            case AlgaeIntake:
+                Actions.Intake_Algae();
+            
+            case Processor:
+                Actions.Outtake_Coral();
+
+            
+
+        }
+    }
+    
 }
+// TODO:
+// Add TODO items lmao
+
+// DONE:
+// state 1 -- L1
+// state 2 -- L2
+// state 3 -- L3
+// state 4 -- L4
+// state 5 -- Coral Intake
+// state 6 -- Algae Intake
+// state 7 -- processor
