d647db9e773af5b838c08d97a711f18d1a1f864f
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2025-01-09 16:36:13 -0600

    Add machine
    Added state machine for states with states

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,17 @@
 package frc.robot;
 
 public class Constants {
-    
+    public enum Machine{
+        L1,
+        L2,
+        L3,
+        L4,
+        CoralIntake,
+        AlgaeIntake,
+        Processor,
+        Ground,
+        Climbing_on,
+        Climbing_off,
+        Robot_off
+    }
 }
