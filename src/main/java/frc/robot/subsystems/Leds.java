343aab79d6f89c80da6dad9bab2b39530be9d7e5
DetermiedMech1 <determiedmech1@gmail.com>
2025-01-15 16:18:50 -0600

    started making LED code

--- src/main/java/frc/robot/subsystems/Leds.java
+++ src/main/java/frc/robot/subsystems/Leds.java
@@ -1,7 +1,18 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
 public class Leds {
-  public Leds() {
-    // Constructor
+  AddressableLED m_led;
+  AddressableLEDBuffer m_ledBuffer;
+
+  int[] coralColor = {255, 255, 255};
+  int[] algaeColor = {0, 255, 255};
+
+  public Leds(int port, int length) {
+    m_led = new AddressableLED(port);
+    m_ledBuffer = new AddressableLEDBuffer(length);
+    m_led.setLength(m_ledBuffer.getLength());
   }
 }
