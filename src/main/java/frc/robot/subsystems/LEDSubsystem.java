a7cffaf50844a716ad10b904c9aa56c89a80d52b
DetermiedMech1 <determiedmech1@gmail.com>
2025-01-16 17:40:14 -0600

    accidentally committed the old file

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -8,10 +8,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
+
   private final AddressableLED m_led;
   private final AddressableLEDBuffer m_buffer;
 
-  public LEDSubsystem(int kPort,int kLength) {
+  public LEDSubsystem(int kPort, int kLength) {
     m_led = new AddressableLED(kPort);
     m_buffer = new AddressableLEDBuffer(kLength);
     m_led.setLength(kLength);
@@ -20,7 +21,7 @@ public class LEDSubsystem extends SubsystemBase {
     // Set the default command to turn the strip off, otherwise the last colors written by
     // the last command to run will continue to be displayed.
     // Note: Other default patterns could be used instead!
-    //setDefaultCommand();
+    setDefaultCommand(runPattern(LEDPattern.solid(Color.kBlack)).withName("Off"));
   }
 
   @Override
