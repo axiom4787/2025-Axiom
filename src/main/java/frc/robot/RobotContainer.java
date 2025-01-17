3b0bef0ce5ba89ae59c1fb21d89d9feeddaca727
coderCH2 <69022677+coderCH2@users.noreply.github.com>
2025-01-17 17:02:07 -0600

    temp yagsl tuning, needs work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,27 @@
 
 package frc.robot;
 
+import java.io.IOException;
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import subsystems.DriveSubsystem;
 
 public class RobotContainer {
+  private final XboxController m_controller = new XboxController(0);
+  private final DriveSubsystem m_driveSubsystem;
+
   public RobotContainer() {
+    m_driveSubsystem = new DriveSubsystem();
     configureBindings();
   }
 
   private void configureBindings() {}
+
+  public Command getTeleopCommand() {
+    return m_driveSubsystem.driveCommand(() -> -m_controller.getLeftY(), () -> -m_controller.getLeftX(), () -> -m_controller.getRightX());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
---
+++ src/main/java/subsystems/DriveSubsystem.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package subsystems;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveSubsystem extends SubsystemBase {
+  private final SwerveDrive swerveDrive;
+
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
+    try {
+      swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(DriveConstants.kMaxSpeedMetersPerSecond);
+    } catch (IOException i) {
+      throw new RuntimeException(i);
+    }
+
+    swerveDrive.setHeadingCorrection(false);
+//    swerveDrive.setCosineCompensator(true);
+
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Gyro Rotation", swerveDrive.getYaw().getDegrees());
+  }
+
+  /**
+   * Command to drive the robot using translative values and heading as angular velocity.
+   *
+   * @param translationX     Translation in the X direction.
+   * @param translationY     Translation in the Y direction.
+   * @param angularRotationX Rotation of the robot to set
+   * @return Drive command.
+   */
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX)
+  {
+    return run(() -> {
+      // Make the robot move
+      swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
+                                          translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity()),
+                        angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity(),
+                        false,
+                        false);
+    });
+  }
+}
