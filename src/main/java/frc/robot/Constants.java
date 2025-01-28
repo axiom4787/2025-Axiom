84328bc8c427b23bfc6e8ec3c27ea0bac8e94bb9
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2025-01-28 16:13:56 -0600

    Patch
    Fixed constants to reflect new designs (semi-WIP) as well as add some constants for the Intake subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,17 +1,49 @@
 package frc.robot;
 
+import org.ejml.equation.Variable;
+
 public class Constants {
+
+    // Global machine state
+    public static Machine state = Machine.Robot_off;
+
+    // Default motor speed
+    public static final float m_default_speed = 1.0f;
+
     public enum Machine{
-        L1,
-        L2,
-        L3AlgaeIntake,
-        L3CoralOuttake,
-        L4AlgaeIntake,
-        L4CoralOuttake,
+        CoralOuttake,
         CoralIntake,
-        AlgaeGroundIntake,
+        AlgaeIntake,
+        AlgaeIntakeGround,
         Processor,
-        ClimbingOn,
-        ElevatorOff
+        L1,
+        Climbing_on,
+        Climbing_off,
+        Robot_off
+    }
+
+    // For Intake Subsystem
+    public static class m_Intake_Motors{
+        public static final int motor_algae_arm = 0; // Check
+        public static final int motor_coral_1 = 0; // Check  
+        public static final int motor_coral_2 = 0; // Check  
+
+        public static final int motor_arm_algae = 0; // Check
+
+        public static final int motor_right_algae = 0; // Check
+        public static final int motor_left_algae = 0; // Check
+    }
+    public static class m_limits{
+        public static final int m_left_coral_limit = 40; // Check limit; should be in amps
+        public static final int m_right_coral_limit = 40; // Check limit; should be in amps
+
+        public static final int m_left_algae_limit = 40; // Check limit; should be in amps
+        public static final int m_right_algae_limit = 40; // Check limit; should be in amps
+
+        public static final int m_arm_algae_limit = 40; // Check limit; should be in amps
+        public static final int m_rot_coral_limit = 40; // Check limit; should be in amps
+
+        public static final int m_arm_algae_1_limit = 40; // Check limit; should be in amps
+        public static final int m_arm_algae_2_limit = 40; // Check limit; should be in amps
     }
 }
