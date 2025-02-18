c5a03e0075e7d88848c31615e17d114a0f088214
DetermiedMech1 <determiedmech1@gmail.com>
2025-02-18 16:15:25 -0600

    improvements #2

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class Constants {
+  public static class ClimberConstants {
+    public static final int CLIMBER_MOTOR_ID = 1;
+    public static final int CLIMBER_MOTOR_CURRENT_LIMIT = 60;
+    public static final double CLIMBER_MOTOR_VOLTAGE_COMP = 12;
+    public static final double CLIMBER_CLIMB_DUTYCYCLE = -0.5;
+    public static final double CLIMBER_UNCLIMB_DUTYCYCLE = 0.5;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,58 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimberConstants;
+
+public class ClimberSubsystem extends SubsystemBase {
+
+  private ClimberState m_state = ClimberState.OFF;
+
+  private final SparkMax climbMotor;
+
+  public ClimberSubsystem() {
+    SparkMaxConfig climbConfig = new SparkMaxConfig();
+
+    climbConfig
+        .voltageCompensation(ClimberConstants.CLIMBER_MOTOR_VOLTAGE_COMP)
+        .smartCurrentLimit(ClimberConstants.CLIMBER_MOTOR_CURRENT_LIMIT)
+        .idleMode(IdleMode.kBrake);
+
+    climbMotor = new SparkMax(ClimberConstants.CLIMBER_MOTOR_ID, MotorType.kBrushless);
+    climbMotor.setCANTimeout(250);
+    climbMotor.configure(climbConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putString("Climber State", m_state.name());
+    switch (m_state) {
+      case OFF:
+        climbMotor.set(0);
+        break;
+      case CLIMB:
+        climbMotor.set(ClimberConstants.CLIMBER_CLIMB_DUTYCYCLE);
+        break;
+      case UNCLIMB:
+        climbMotor.set(ClimberConstants.CLIMBER_UNCLIMB_DUTYCYCLE);
+        break;
+    }
+  }
+
+  public void setState(ClimberState desiredState) {
+    m_state = desiredState;
+  }
+
+  public enum ClimberState {
+    OFF,
+    CLIMB,
+    UNCLIMB
+  }
+}
